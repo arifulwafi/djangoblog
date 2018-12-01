48cdc91d5c62ec32d10abd75783fbae058743ad9
Hasan Mahmud <linckon@live.com>
2018-12-02 01:06:47 +0600

    blog crud added

---
+++ src/DjangoWeb/blog/forms.py
@@ -0,0 +1,8 @@
+﻿from django.forms import ModelForm
+from .models import BlogPost
+
+
+class BlogForm(ModelForm):
+    class Meta:
+        model = BlogPost
+        exclude =['status','isdeleted','order','created_date','last_updated_date']
--- src/DjangoWeb/blog/views.py
+++ src/DjangoWeb/blog/views.py
@@ -9,20 +9,21 @@ from django.core.urlresolvers import reverse
 from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext
-from django.utils import timezone
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from os import path
+from .forms import BlogForm
 
 import json
+
 
 class BlogPostListView(ListView):
     model = BlogPost
 
     def get_context_data(self, **kwargs):
         context = super(BlogPostListView, self).get_context_data(**kwargs)
-        context['title'] = 'BlogPosts'
+        context['title'] = 'Blog Posts'
         context['year'] = datetime.now().year
         return context
 
@@ -31,7 +32,7 @@ class BlogPostDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(BlogPostDetailView, self).get_context_data(**kwargs)
-        context['title'] = 'BlogPost'
+        context['title'] = 'Post Detail'
         context['year'] = datetime.now().year
         return context
 
@@ -42,15 +43,35 @@ class AuthorBlogPostView(DetailView):
     model = BlogPost
 
 class AuthorBlogPostCreate(CreateView):
-    model = BlogPost
-    fields = ['title']
-    success_url = reverse_lazy('author_blog_post_list')
+    template_name = "blog/author_blog_post_form.html"
+    form_class = BlogForm
+
+    def form_valid(self,form):
+        form.instance.status = 1
+        form.instance.isdeleted = False
+        form.instance.created_date = datetime.now()
+        form.instance.last_updated_date = datetime.now()
+
+        return super(AuthorBlogPostCreate,self).form_valid(form)
+
+    success_url = reverse_lazy('blog:author_blog_post_list')
+    
 
 class AuthorBlogPostUpdate(UpdateView):
+    template_name = "blog/author_blog_post_form.html"
     model = BlogPost
-    fields = ['title', 'short_description', 'content']
-    success_url = reverse_lazy('author_blog_post_list')
+    form_class = BlogForm
+
+    def form_valid(self,form):
+        form.instance.status = 1
+        form.instance.isdeleted = False
+        form.instance.created_date = datetime.now()
+        form.instance.last_updated_date = datetime.now()
+
+        return super(AuthorBlogPostUpdate,self).form_valid(form)
+
+    success_url = reverse_lazy('blog:author_blog_post_list')
 
 class AuthorBlogPostDelete(DeleteView):
     model = BlogPost
-    success_url = reverse_lazy('author_blog_post_list')
+    success_url = reverse_lazy('blog:author_blog_post_list')
